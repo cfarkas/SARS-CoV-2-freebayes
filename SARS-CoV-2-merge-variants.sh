996a694a6eb1178d7111cbab6d102e11bcda963f
cfarkas <carlosfarkas@gmail.com>
2020-12-13 14:07:57 -0600

    Update SARS-CoV-2-merge-variants.sh

--- SARS-CoV-2-merge-variants.sh
+++ SARS-CoV-2-merge-variants.sh
@@ -72,7 +72,7 @@ begin=`date +%s`
 ### Merge VCFs using jacquard
 echo "Merge VCFs using jacquard"
 echo ""
-ulimit -n 1000000 && jacquard merge --include_all ./ merged.GISAID.vcf
+jacquard merge --include_all ./ merged.GISAID.vcf
 echo ""
 # Left only genotypes in merged VCF
 echo "Fixing genotypes in merged VCF"
