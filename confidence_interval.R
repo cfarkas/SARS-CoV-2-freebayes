e9e16c5525900e33fcfdca513a4a15826d5016a0
cfarkas <carlosfarkas@gmail.com>
2021-01-08 15:22:22 -0600

    to select binned pi-Tajima's D values outside 95CI
    script to select binned pi-Tajima's D values outside 95CI

--- confidence_interval.R
+++ confidence_interval.R
@@ -17,10 +17,10 @@ package.check <- lapply(
 bins<-read.table("input.pi.D", header=F)
 names(bins)[names(bins) == "V2"] <- "nucleotide_diversity"
 names(bins)[names(bins) == "V3"] <- "D"
-lower_limit=quantile(bins$D, .025)
-lower_limit
-upper_limit=quantile(bins$D, .975)
+upper_limit=quantile(bins$D, .025)
 upper_limit
+lower_limit=quantile(bins$D, .975)
+lower_limit
 pdf("pi_tajima.pdf")
 gg <- ggplot(bins, aes(x=nucleotide_diversity, y=D))+
 geom_point()+
@@ -29,7 +29,7 @@ geom_point()+
 ### geom_label_repel
 gg + 
   geom_label_repel(aes(label = V1),
-                  data          = subset(bins, D > upper_limit),
+                  data          = subset(bins, D < upper_limit),
                   size          = 2.5,
                   box.padding   = 0.4,
                   point.padding = 0.4,
@@ -38,7 +38,7 @@ gg +
                   direction     = "x",
                   max.overlaps  = Inf) +
   geom_label_repel(aes(label = V1),
-                  data         = subset(bins, D < lower_limit),
+                  data         = subset(bins, D > lower_limit),
                   size          = 2.5,
                   box.padding   = 0.4,
                   point.padding = 0.4,
@@ -56,8 +56,8 @@ upper_bins_subset <- subset(bins, bins$D > upper_limit)
 newdata_upper <- upper_bins_subset[myvars]
 lower_bins_subset <- subset(bins, bins$D < lower_limit)
 newdata_lower <- lower_bins_subset[myvars]
-write.table(newdata_upper, file="bins_97.5%_confidence.tab", sep="\t", quote = FALSE, row.names = FALSE)
-write.table(newdata_lower, file="bins_2.5%_confidence.tab", sep="\t", quote = FALSE, row.names = FALSE)
+write.table(newdata_upper, file="bins_2.5%_confidence.tab", sep="\t", quote = FALSE, row.names = FALSE)
+write.table(newdata_lower, file="bins_97.5%_confidence.tab", sep="\t", quote = FALSE, row.names = FALSE)
 proc.time()
 sessionInfo()
 quit("no")
