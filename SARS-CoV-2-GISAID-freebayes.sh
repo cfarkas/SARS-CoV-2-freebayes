6cbc28bd9952b8a0d7fd166a4e5fadbfb7d2a66b
cfarkas <carlosfarkas@gmail.com>
2021-01-17 16:11:17 -0600

    update ulimit -s to 1000000
    update ulimit -s to 1000000

--- SARS-CoV-2-GISAID-freebayes.sh
+++ SARS-CoV-2-GISAID-freebayes.sh
@@ -97,7 +97,7 @@ echo ""
 ### Align fasta files to reference (covid19-refseq.fasta, provided in this repository) and call variants with freebayes (option C 1)
 echo "Aligning fasta files to reference and call variants with freebayes (option C 1)"
 echo ""
-ulimit -n 1000000 && ulimit -s 299999 
+ulimit -n 1000000 && ulimit -s 1000000 
 samtools faidx ${2}
 fasta= ls -1 EPI*.fasta
 for fasta in EPI*.fasta; do
