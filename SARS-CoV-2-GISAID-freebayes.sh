be1c8828886b4fcafa513f41a94508ba0130adf2
cfarkas <carlosfarkas@gmail.com>
2021-03-11 22:40:08 -0600

    Update SARS-CoV-2-GISAID-freebayes.sh
    adding getopts

--- SARS-CoV-2-GISAID-freebayes.sh
+++ SARS-CoV-2-GISAID-freebayes.sh
@@ -1,76 +1,29 @@
 #!/bin/bash
 set -e 
-
 {
+usage="$(basename "$0") [-h] [-f <GISAID.fasta>] [-g <reference_genome.fasta>] [-t <threads>]
+This program will call variants using freebayes in given GISAID fasta sequences files to obtain major viral variants.
+Arguments:
+    -h  show this help text
+    -f  GISAID genomes in FASTA format
+    -g  PATH where the SARS-CoV-2 reference genome (in fasta format) is located. If the genome is located in the working folder, just specify the name.
+    -t  Number of CPU processors"
+options=':hf:g:t:'
+while getopts $options option; do
+  case "$option" in
+    h) echo "$usage"; exit;;
+    f) f=$OPTARG;;
+    g) g=$OPTARG;;
+    t) t=$OPTARG;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
+  esac
+done
 
-GISAID_fasta=${1}
-Reference=${2}
-Threads=${3}
-
-if [ "$1" == "-h" ]; then
-  echo ""
-  echo "Usage: ./`basename $0` [GISAID_fasta] [Reference] [Threads]"
-  echo ""
-  echo "This program will call variants using freebayes in given GISAID fasta sequences files to obtain major viral variants."
-  echo ""
-  echo "[GISAID_fasta] : GISAID genomes in FASTA format"
-  echo ""
-  echo "[Reference]: PATH where the SARS-CoV-2 reference genome (in fasta format) is located. If the genome is located in the working folder, just specify the name."
-  echo ""
-  echo "[Threads]: Number of CPUs for the task (integer)"
-  echo ""
-  exit 0
-fi
-
-if [ "$1" == "-help" ]; then
-  echo ""
-  echo "Usage: ./`basename $0` [GISAID_fasta] [Reference] [Threads]"
-  echo ""
-  echo "This program will call variants using freebayes in given GISAID fasta sequences files to obtain major viral variants."
-  echo ""
-  echo "[GISAID_fasta] : GISAID genomes in FASTA format"
-  echo ""
-  echo "[Reference]: PATH where the SARS-CoV-2 reference genome (in fasta format) is located. If the genome is located in the working folder, just specify the name."
-  echo ""
-  echo "[Threads]: Number of CPUs for the task (integer)"
-  echo ""
-  exit 0
-fi
-if [ "$1" == "--h" ]; then
-  echo ""
-  echo "Usage: ./`basename $0` [GISAID_fasta] [Reference] [Threads]"
-  echo ""
-  echo "This program will call variants using freebayes in given GISAID fasta sequences files to obtain major viral variants."
-  echo ""
-  echo "[GISAID_fasta] : GISAID genomes in FASTA format"
-  echo ""
-  echo "[Reference]: PATH where the SARS-CoV-2 reference genome (in fasta format) is located. If the genome is located in the working folder, just specify the name."
-  echo ""
-  echo "[Threads]: Number of CPUs for the task (integer)"
-  echo ""
-  exit 0
-fi
-
-if [ "$1" == "--help" ]; then
-  echo ""
-  echo "Usage: ./`basename $0` [GISAID_fasta] [Reference] [Threads]"
-  echo ""
-  echo "This program will call variants using freebayes in given GISAID fasta sequences files to obtain major viral variants."
-  echo ""
-  echo "[GISAID_fasta] : GISAID genomes in FASTA format"
-  echo ""
-  echo "[Reference]: PATH where the SARS-CoV-2 reference genome (in fasta format) is located. If the genome is located in the working folder, just specify the name."
-  echo ""
-  echo "[Threads]: Number of CPUs for the task (integer)"
-  echo ""
-  exit 0
-fi
-
-[ $# -eq 0 ] && { echo "Usage: ./`basename $0` [GISAID_fasta] [Reference] [Threads]"; exit 1; }
-
-if [ $# -ne 3 ]; then
-  echo 1>&2 "Usage: ./`basename $0` [GISAID_fasta] [Reference] [Threads]"
-  exit 3
+# mandatory arguments
+if [ ! "$f" ] || [ ! "$g" ] || [ ! "$t" ]; then
+  echo "arguments -f, -g and -t must be provided"
+  echo "$usage" >&2; exit 1
 fi
 
 begin=`date +%s`
@@ -78,13 +31,13 @@ begin=`date +%s`
 ### Split fasta files
 echo "fixing names in FASTA file"
 echo ""
-sed -i 's/ /-/'g ${1}
-sed -i "s|hCoV-19/.*./2021||"g ${1}
-sed -i "s|hCoV-19/.*./2020||"g ${1}
-sed -i "s|hCoV-19/.*./2019||"g ${1}
-sed -i 's/|/\t/'g ${1}
-sed -i 's/>\t/>/'g ${1}
-seqkit fx2tab ${1} > merged.GISAID.tabular
+sed -i 's/ /-/'g ${f}
+sed -i "s|hCoV-19/.*./2021||"g ${f}
+sed -i "s|hCoV-19/.*./2020||"g ${f}
+sed -i "s|hCoV-19/.*./2019||"g ${f}
+sed -i 's/|/\t/'g ${f}
+sed -i 's/>\t/>/'g ${f}
+seqkit fx2tab ${f} > merged.GISAID.tabular
 awk '{print $1"\t"$NF}' merged.GISAID.tabular > merged.GISAID.tab && rm merged.GISAID.tabular
 grep "EPI_" merged.GISAID.tab > merged.GISAID.tabular && rm merged.GISAID.tab
 seqkit tab2fx merged.GISAID.tabular > merged.GISAID.fasta && rm merged.GISAID.tabular
@@ -98,14 +51,14 @@ echo ""
 echo "Aligning fasta files to reference and call variants with freebayes (option C 1)"
 echo ""
 ulimit -n 1000000 && ulimit -s 1000000 
-samtools faidx ${2}
+samtools faidx ${g}
 fasta= ls -1 EPI*.fasta
 for fasta in EPI*.fasta; do
-minimap2 -ax asm5 -t ${3} ${2} ${fasta} > ${fasta}.sam
+minimap2 -ax asm5 -t ${t} ${g} ${fasta} > ${fasta}.sam
 samtools view -bS ${fasta}.sam > ${fasta}.bam
 samtools sort -o ${fasta}.sorted.bam ${fasta}.bam
-freebayes -f ${2} -C 1 ${fasta}.sorted.bam > ${fasta}.vcf
-vcfleftalign -r ${2} ${fasta}.vcf > ${fasta}.left.vcf
+freebayes -f ${g} -C 1 ${fasta}.sorted.bam > ${fasta}.vcf
+vcfleftalign -r ${g} ${fasta}.vcf > ${fasta}.left.vcf
 rm ${fasta}.sam ${fasta}.bam ${fasta}.sorted.bam ${fasta}.vcf
 done
 echo "Done. Continue with variant aggregation"
@@ -152,7 +105,7 @@ sed -i 's/NC_04551202/NC_045512.2/'g merged.GISAID.fixed.vcf
 # left-align vcf file and fix names
 echo "left-aligning vcf file and fixing names"
 echo ""
-vcfleftalign -r ${2} merged.GISAID.fixed.vcf > merged.GISAID.left.vcf
+vcfleftalign -r ${g} merged.GISAID.fixed.vcf > merged.GISAID.left.vcf
 sed -i 's/|unknown//'g merged.GISAID.left.vcf
 
 # Merge VCFs using vcfcombine, see combined_sites.raw.vcf file
@@ -166,8 +119,8 @@ echo ""
 vcffixup merged.GISAID.left.vcf > merged.GISAID.AF.raw.vcf
 wget https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/master/problematic_sites_sarsCov2.vcf
 sed -i 's/MN908947.3/NC_045512.2/'g problematic_sites_sarsCov2.vcf
-vcfintersect -i problematic_sites_sarsCov2.vcf merged.GISAID.AF.raw.vcf -r ${2} --invert > merged.GISAID.AF.vcf
-vcfintersect -i problematic_sites_sarsCov2.vcf combined_sites.raw.vcf -r ${2} --invert > combined_sites.vcf
+vcfintersect -i problematic_sites_sarsCov2.vcf merged.GISAID.AF.raw.vcf -r ${g} --invert > merged.GISAID.AF.vcf
+vcfintersect -i problematic_sites_sarsCov2.vcf combined_sites.raw.vcf -r ${g} --invert > combined_sites.vcf
 rm merged.GISAID.fixed.vcf merged.GISAID.left.vcf
 gzip merged.GISAID.vcf merged.GISAID.AF.raw.vcf combined_sites.raw.vcf
 gzip *.fasta 
